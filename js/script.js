d9ff7b1ed3205ec1f36c2a7b0432bd87c5913b27
FelipeSantos-cco <felipe.almeida@sptech.school>
2023-04-11 20:38:12 -0300

    Format Document

--- js/script.js
+++ js/script.js
@@ -26,49 +26,49 @@ function validarContato() {
     var whats = ipt_wpp.value;
     var email = ipt_email.value;
     var emailConfirm = ipt_emailConfirm.value;
-    
+
     var valida_telefone = "";
     var valida_whats = "";
     var valida_email = "";
 
     // Validações
-    valida_telefone = telefone.length >= 8 &&  telefone.length <= 13;
-    valida_whats = whats.length >= 8 &&  whats.length <= 12;
+    valida_telefone = telefone.length >= 8 && telefone.length <= 13;
+    valida_whats = whats.length >= 8 && whats.length <= 12;
     valida_email = email == emailConfirm;
 
     // Verificações
-    if(valida_telefone){
-        if(valida_whats){
-            if(valida_email){
+    if (valida_telefone) {
+        if (valida_whats) {
+            if (valida_email) {
                 // Todos os campos estão corretos :) !
                 alert(`Todos os campos estão corretos :)`);
-            
-            }else{
+
+            } else {
                 alert(`Os E-mails não podem ser diferentes!`);
                 ipt_emailConfirm.focus()
             }
 
-        }else if(typeof whats == "string"){
+        } else if (typeof whats == "string") {
             alert(`Não aceitamos letras no campo de WhatsApp!`);
             ipt_wpp.focus();
-        
-        }else{
+
+        } else {
             alert(`Por favor, insira o WhatsApp corretamente!`);
             ipt_wpp.focus();
         }
 
-    }else if(typeof telefone == "string"){
+    } else if (typeof telefone == "string") {
         alert(`Não aceitamos letras no campo de telefone!`);
         ipt_telefone.focus();
-        
-    }else{
+
+    } else {
         alert(`Por favor, insira o telefone corretamente!`);
         ipt_telefone.focus();
-        
+
     }
 }
 
 // Função para chamar outras funções que são de validação 
-function validar(){
+function validar() {
     validarContato();
 }
