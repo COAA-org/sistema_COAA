63ec65173b19ed2893c2a593df142d9b7316b471
FelipeSantos-cco <felipe.almeida@sptech.school>
2023-04-11 20:29:53 -0300

    Nova feat
    ./cadastro.html: Modificação nos campos que pertencem ao "Contato da empresa" e no button para chamar a função `validar()`;
    
    ./js/script.js: Adicionando duas funções, são elas:
    
    	- `validar()` Responsável por chamar as funções de validação no Cadastro da empresa;
    	- `validarContato()` Responsável por fazer verificações nos campos que pertencem ao "Contato da empresa";

--- js/script.js
+++ js/script.js
@@ -19,3 +19,56 @@ function validarLogin() {
         ipt_email.focus();
     }
 }
+
+function validarContato() {
+    // Variaveis
+    var telefone = ipt_telefone.value;
+    var whats = ipt_wpp.value;
+    var email = ipt_email.value;
+    var emailConfirm = ipt_emailConfirm.value;
+    
+    var valida_telefone = "";
+    var valida_whats = "";
+    var valida_email = "";
+
+    // Validações
+    valida_telefone = telefone.length >= 8 &&  telefone.length <= 13;
+    valida_whats = whats.length >= 8 &&  whats.length <= 12;
+    valida_email = email == emailConfirm;
+
+    // Verificações
+    if(valida_telefone){
+        if(valida_whats){
+            if(valida_email){
+                // Todos os campos estão corretos :) !
+                alert(`Todos os campos estão corretos :)`);
+            
+            }else{
+                alert(`Os E-mails não podem ser diferentes!`);
+                ipt_emailConfirm.focus()
+            }
+
+        }else if(typeof whats == "string"){
+            alert(`Não aceitamos letras no campo de WhatsApp!`);
+            ipt_wpp.focus();
+        
+        }else{
+            alert(`Por favor, insira o WhatsApp corretamente!`);
+            ipt_wpp.focus();
+        }
+
+    }else if(typeof telefone == "string"){
+        alert(`Não aceitamos letras no campo de telefone!`);
+        ipt_telefone.focus();
+        
+    }else{
+        alert(`Por favor, insira o telefone corretamente!`);
+        ipt_telefone.focus();
+        
+    }
+}
+
+// Função para chamar outras funções que são de validação 
+function validar(){
+    validarContato();
+}
